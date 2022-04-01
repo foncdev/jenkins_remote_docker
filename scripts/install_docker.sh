7b358cff07ce67ddbbcdef3644cf130e97bd4b45
foncdev <43604336+foncdev@users.noreply.github.com>
2022-04-01 13:21:33 +0900

    Update install_docker.sh
    zip add

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y apt-transport-https \
-       ca-certificates curl gnupg2 \
+       ca-certificates curl gnupg2 zip \
        software-properties-common
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
